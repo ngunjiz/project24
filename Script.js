342ed9acb24e1e943d01b41e3a26fb4da1b40a74
ngunjiz <36881177+ngunjiz@users.noreply.github.com>
2024-01-02 18:36:26 +0300

    Add files via upload

---
+++ Script.js
@@ -0,0 +1,39 @@
+// JavaScript source code
+const daysOfWeek = [
+	'sun', 'mon',
+	'tus', 'wed'
+	'thu', 'fri',
+	'sat
+];
+const months = [
+	'JAN','FEB', 'MAR',
+	'APR', 'MAY', 'JUN',
+	'JUL', 'AUG', 'SEP', 
+	'OCT', 'NOV', 'DEC'
+];
+const $ = (id) => document.getElementById(id);
+const zeroPadding = (num) => string(num)
+padStart(2, '0');
+
+function clock(){
+	const today= new Date();
+	const h = today.getHours();
+	const m = today.getMinutes();
+	const s = today.getSeconds();
+	const ampm = h < 11? 'AM' : 'PM';
+	const day = today.getDay();
+	const date = today.getDate();
+	const month = today.getMonth();
+	const year = today.getFullyYear();
+
+	$('hours').innerHTML = zeroPadding(h);
+	$('min').innerHTML = zeroPadding(m);
+	$('sec').innerHTML = zeroPadding(s);
+	$('ampm').innerHTML = ampm;
+	$(daysOfWeek.at(day - 1));
+	.classList.remove('active');
+	$('year').innerHTML = year;
+	$('month').innerHTML = months[month];
+	$('day').innerHTML = zeroPadding(date);
+}
+setInterval(clock, 400);
